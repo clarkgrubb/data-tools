8b041fd4af958e2558824fdaad92a77848e14e8e
Clark Grubb <clarkgrubb@gmail.com>
2014-04-26 10:11:26 -0700

    slightly safer undermongo

--- src/undermongo.sh
+++ src/undermongo.sh
@@ -2,17 +2,27 @@
 
 set -eu -o pipefail
 
-underscore_url=http://underscorejs.org/underscore.js
+readonly underscore_url=http://underscorejs.org/underscore.js
 if [ -d ~/Local/src ]
 then
     underscore_file=~/Local/src/underscore.js
+    cleanup_underscore_file=0
 else
-    underscore_file=/tmp/underscore.js
+    # Can't use mktemp. :(
+    # File must have .js suffix.
+    underscore_file=/tmp/underscore.$$.js
+    rm -f $underscore_file
+    cleanup_underscore_file=1
 fi
 
 if [ ! -e $underscore_file ]
 then
-  curl $underscore_url > $underscore_file
+  curl -s $underscore_url > $underscore_file
 fi
 
 mongo --shell "$@" $underscore_file
+
+if [ $cleanup_underscore_file ]
+then
+    rm -f $underscore_file
+fi
