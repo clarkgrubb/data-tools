284cca0596ffada9630ca9ab9907e84dce8825e4
Clark Grubb <clarkgrubb@gmail.com>
2015-11-28 20:04:09 -0800

    make tab-to-csv -u work

--- src/tab-to-csv/tab_to_csv.c
+++ src/tab-to-csv/tab_to_csv.c
@@ -1,3 +1,4 @@
+#include <getopt.h>
 #include <locale.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -34,19 +35,42 @@ write_wchars(wchar_t buf[], size_t start, size_t end, bool escape,
   }
 }
 
-int
-main(int argc, char **argv) {
+int tab_to_csv(bool unescape_char) {
   wchar_t ch;
   wchar_t buf[BUFSIZE];
   size_t i = 0;
   size_t write_i = 0;
   bool escape_field = false;
   bool open_field = true;
+  bool last_char_backslash = false;
 
-  setlocale(LC_ALL, "");
-
-  while ((ch = getwchar()) != WEOF) {
+ while ((ch = getwchar()) != WEOF) {
     switch(ch) {
+
+    case L'\\':
+      if (last_char_backslash) {
+        if (!unescape_char) {
+          buf[IDX(i)] = L'\\';
+          ++i;
+          if (i - write_i >= BUFSIZE) {
+            escape_field = true;
+            write_wchars(buf, write_i, i - 1, escape_field, open_field, false);
+            open_field = false;
+            write_i = i;
+          }
+        }
+        buf[IDX(i)] = L'\\';
+        ++i;
+        if (i - write_i >= BUFSIZE) {
+          escape_field = true;
+          write_wchars(buf, write_i, i - 1, escape_field, open_field, false);
+          open_field = false;
+          write_i = i;
+        }
+      }
+      last_char_backslash = !last_char_backslash;
+      break;
+
     case L'\t':
       write_wchars(buf, write_i, i - 1, escape_field, open_field, true);
       putwchar(L',');
@@ -54,6 +78,7 @@ main(int argc, char **argv) {
       open_field = true;
       escape_field = false;
       break;
+
     case L'\n':
       write_wchars(buf, write_i, i - 1, escape_field, open_field, true);
       putwchar(L'\r');
@@ -62,11 +87,113 @@ main(int argc, char **argv) {
       open_field = true;
       escape_field = false;
       break;
+
+    case L't':
+      if (unescape_char && last_char_backslash)
+        buf[IDX(i)] = L'\t';
+      else if (last_char_backslash) {
+        buf[IDX(i)] = L'\\';
+        ++i;
+        if (i - write_i >= BUFSIZE) {
+          escape_field = true;
+          write_wchars(buf, write_i, i - 1, escape_field, open_field, false);
+          open_field = false;
+          write_i = i;
+        }
+        buf[IDX(i)] = L't';
+      }
+      else {
+        buf[IDX(i)] = L't';
+      }
+      last_char_backslash = false;
+      ++i;
+      if (i - write_i >= BUFSIZE) {
+        escape_field = true;
+        write_wchars(buf, write_i, i - 1, escape_field, open_field, false);
+        open_field = false;
+        write_i = i;
+      }
+      break;
+
+    case L'n':
+      if (unescape_char && last_char_backslash) {
+        buf[IDX(i)] = L'\n';
+        escape_field = true;
+      }
+      else if (last_char_backslash) {
+        buf[IDX(i)] = L'\\';
+        ++i;
+        if (i - write_i >= BUFSIZE) {
+          escape_field = true;
+          write_wchars(buf, write_i, i - 1, escape_field, open_field, false);
+          open_field = false;
+          write_i = i;
+        }
+        buf[IDX(i)] = L'n';
+      }
+      else {
+        buf[IDX(i)] = L'n';
+      }
+      last_char_backslash = false;
+      ++i;
+      if (i - write_i >= BUFSIZE) {
+        escape_field = true;
+        write_wchars(buf, write_i, i - 1, escape_field, open_field, false);
+        open_field = false;
+        write_i = i;
+      }
+      break;
+
+    case L'r':
+      if (unescape_char && last_char_backslash) {
+        buf[IDX(i)] = L'\r';
+        escape_field = true;
+      }
+      else if (last_char_backslash) {
+        buf[IDX(i)] = L'\\';
+        ++i;
+        if (i - write_i >= BUFSIZE) {
+          escape_field = true;
+          write_wchars(buf, write_i, i - 1, escape_field, open_field, false);
+          open_field = false;
+          write_i = i;
+        }
+        buf[IDX(i)] = L'r';
+      }
+      else {
+        buf[IDX(i)] = L'r';
+      }
+      last_char_backslash = false;
+      ++i;
+      if (i - write_i >= BUFSIZE) {
+        escape_field = true;
+        write_wchars(buf, write_i, i - 1, escape_field, open_field, false);
+        open_field = false;
+        write_i = i;
+      }
+      break;
+
     case L'"':
     case L',':
     case L'\r':
       escape_field = true;
     default:
+      if (last_char_backslash) {
+        if (unescape_char) {
+          fprintf(stderr, "unexpected backslash");
+          exit(1);
+        }
+        else {
+          buf[IDX(i)] = L'\\';
+          ++i;
+          if (i - write_i >= BUFSIZE) {
+            escape_field = true;
+            write_wchars(buf, write_i, i - 1, escape_field, open_field, false);
+            open_field = false;
+            write_i = i;
+          }
+        }
+      }
       buf[IDX(i)] = ch;
       ++i;
       if (i - write_i >= BUFSIZE) {
@@ -83,4 +210,35 @@ main(int argc, char **argv) {
     return 1;
   }
   return 0;
+}
+
+int
+main(int argc, char **argv) {
+  static struct option long_opts[] = {
+    {"unescape", no_argument, NULL, 'u'},
+    {0, 0, 0, 0}
+  };
+
+  int opti;
+  bool unescape_char = false;
+
+  setlocale(LC_ALL, "");
+
+  while (1) {
+    int ch = getopt_long(argc, argv, "u", long_opts, &opti);
+    if (-1 == ch) {
+      break;
+    }
+
+    switch (ch) {
+    case 'u':
+      unescape_char = true;
+      break;
+    default:
+      fprintf(stderr, "unexpected arg: %d\n", ch);
+      exit(1);
+    }
+  }
+
+  return tab_to_csv(unescape_char);
 }
