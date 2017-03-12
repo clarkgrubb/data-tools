19e1b6c6e292277ddc3809a1d3d7c8f3c41cecfa
Clark Grubb <clarkgrubb@gmail.com>
2017-03-12 12:47:38 -0700

    implement dt help

--- src/dt.sh
+++ src/dt.sh
@@ -2,7 +2,10 @@
 
 if [ $# -lt 1 ]
 then
-    echo 'IMPLEMENT ME: list of commands' >&2
+    echo 'USAGE: dt SUBCOMMAND [ARGS] ...' >&2
+    echo '' >&2
+    echo 'subcommands:' >&2
+    ( cd bin && ls ) >&2
     exit 0
 else
     cmd=$1
